9ab5ce57bedd4cc067fcaa03a2195a3b8112b01a
Friedel Wolff <friedel@translate.org.za>
2019-02-08 10:47:57 +0200

    Update version in setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ else:
 
 setup(
     name='django-compression-middleware',
-    version='0.1.0',
+    version='0.2.1',
     description="""Django middleware to compress responses using several algorithms.""",
     long_description=io.open("README.rst", 'r', encoding="utf-8").read(),
     url='https://github.com/friedelwolff/django-compresion-middleware',
