2ae3935f88ee92bf4912383634d8fcfe28167e7b
Helmut Irle <helmut@devigninnovation.com>
2019-10-22 19:56:59 +0200

    Added decorator to __all__.

--- compression_middleware/decorators.py
+++ compression_middleware/decorators.py
@@ -1,10 +1,10 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+__all__ = ['compress_page', 'compress_exempt']
+
 from functools import wraps
-
-__all__ = ['compress_page']
-
 
 from .middleware import CompressionMiddleware
 from django.utils.decorators import decorator_from_middleware
